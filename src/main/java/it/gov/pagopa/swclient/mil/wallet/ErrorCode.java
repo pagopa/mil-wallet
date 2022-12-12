e046a976437e646374b75831f04321296b402536
antoniotarricone <antonio.tarricone@pagopa.it>
2022-12-12 17:01:23 +0100

    First commit.

--- src/main/java/it/gov/pagopa/swclient/mil/wallet/ErrorCode.java
+++ src/main/java/it/gov/pagopa/swclient/mil/wallet/ErrorCode.java
@@ -4,14 +4,14 @@
  * 12 dec 2022
  */
 
-package it.gov.pagopa.swclient.mil.__service_name__;
+package it.gov.pagopa.swclient.mil.wallet;
 
 /**
  * 
  * @author Antonio Tarricone
  */
 public final class ErrorCode {
-	public static final String MODULE_ID = "002";
+	public static final String MODULE_ID = "003";
 
 	private ErrorCode() {
 	}
